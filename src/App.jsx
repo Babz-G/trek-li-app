3794b047ad858a0d33772a39d22d246173cb553e
Babz-G <barbaramgaynor@gmail.com>
2026-02-20 01:51:16 -0500

    initial file setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Compnents/navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Compnents/navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>Trek Long Island</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
