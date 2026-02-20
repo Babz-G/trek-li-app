0fbbd77f6bd3e3d7f0cac2ad394190f1a5d5759c
Babz-G <barbaramgaynor@gmail.com>
2026-02-20 02:13:00 -0500

    navbar setup

--- src/Compnents/navbar.jsx
+++ src/Compnents/navbar.jsx
@@ -1,7 +1,16 @@
+import logo from "../images/trek-li-logo.jpeg";
+
 function Navbar() {
   return (
-    <nav>
-      <h1>Trek Long Island</h1>
+    <nav className="navbar">
+      <img src={logo} alt="Trek Long Island Logo" className="navbar-logo" />
+      <ul className="navbar-links">
+        <li>Home</li>
+        <li>Schedule</li>
+        <li>Celebrity Guests</li>
+        <li>Venue Map</li>
+        <li>Sponsors</li>
+      </ul>
     </nav>
   );
 }
